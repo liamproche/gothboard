7bd66ad54ff60ff0467ef7e0a53e9b2f9d51107b
mac <mac@WDWWW.local>
2024-02-24 10:13:26 -0500

    work on Auth

--- src/MainComponent/MainComponent.jsx
+++ src/MainComponent/MainComponent.jsx
@@ -1,7 +1,29 @@
 import './MainComponent.css';
+import { useState } from 'react';
 
 function MainComponent() {
-  return (
+    const [auth, setAuth] = useState({})
+    const getAuth = async(code) =>{
+        try{
+            const authResponse = await fetch("https://api.streamelements.com/oauth2/authorize" + 
+            new URLSearchParams({
+               'client_id': process.env.SE_CLIENT_ID,
+               'client_secret': process.env.CLIENT_SECRET,
+               'grant_type': 'authorization_code',
+               'code': code,
+               'redirect_uri': REDIRECT_URI
+            })
+            )
+            console.log('Authentication data: ', data)
+        }catch(err){
+            console.log(err)
+        }
+    }
+    getAuth(code)
+  
+  
+  
+    return (
    <div className='MainComponent'>
     <h1>This is the MainComponent</h1>
    </div>

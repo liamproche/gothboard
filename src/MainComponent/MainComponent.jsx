a1b079ae98c7c1032004e64ad61fa6c729517493
mac <mac@WDWWW.local>
2024-02-24 08:54:15 -0500

    added MainComponent

---
+++ src/MainComponent/MainComponent.jsx
@@ -0,0 +1,11 @@
+import './MainComponent.css';
+
+function MainComponent() {
+  return (
+   <div className='MainComponent'>
+    <h1>This is the MainComponent</h1>
+   </div>
+  );
+}
+
+export default MainComponent;
